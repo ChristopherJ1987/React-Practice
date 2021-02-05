ec050748f82c9da14f65a5c26c6e5f9ff3bb66ef
Christopher J <chrisbjones1987@yahoo.com>
2021-02-05 08:58:47 -0600

    Added a line to App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,3 +1,5 @@
+// I was still having a difficult time setting this app up on my own, and knowing when and where to use components so this code was mostly copied from Brian Design on YouTube...
+
 import React from 'react';
 import './App.css';
 import Todolist from './Components/Todolist';
